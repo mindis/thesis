d7a74271fbb0c660cd875f40ae2c995562d8fcc1
nggianno <nick_gianno@hotmail.com>
2020-02-14 14:32:21 +0200

    update projects

--- cosmetics-shop/build_dataset.py
+++ cosmetics-shop/build_dataset.py
@@ -3,8 +3,26 @@ import pandas as pd
 import sklearn
 from sklearn import cluster
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
+def get_statistics(df):
+    # Get event types
+    event_types = df['event_type'].unique()
+    print(event_types)
+    # Get frequences of event types
+    event_types_freq = df['event_type'].value_counts()
+    print(event_types_freq)
+    event_types_freq = pd.DataFrame(event_types_freq)
+    event_types_freq.rename(columns={"event_type": "frequencies"}, inplace=True)
+    event_types_freq.index.name = 'event_type'
+    print(event_types_freq)
 
+    # visualize frequencies
+    plt.xlabel('Event Type')
+    plt.ylabel('Number of Events in Dataset')
+    plt.bar(range(len(event_types_freq)), event_types_freq['frequencies'])
+    plt.xticks(range(len(event_types_freq)), event_types, rotation='vertical')
+    plt.show()
 
 def build_dataset_for_rnn(df):
 
@@ -40,39 +58,46 @@ def build_dataset_with_ratings(df, num):
     # df = pd.Dataframe(user_item_df)
     df = pd.DataFrame(df[:num])
     print(df)
-    ratings_df = df.groupby(by=['user_id', 'itemid']).sum()
+    ratings_df = df.groupby(by=['user_id', 'product_id']).sum()
     ratings_df = pd.DataFrame(data=ratings_df)
 
     return ratings_df
 
 
 if __name__ == "__main__":
+
     PATH = '/home/nick/Desktop/thesis/datasets/cosmetics-shop-data/cleaned_data.csv'
     data = pd.read_csv(PATH)  # 2019-Nov.csv for November records
     print(data)
 
+    #get_statistics(data)
+
+
     user_item_matrix = data.groupby(['user_id', 'product_id']).event_type.value_counts().to_frame()
     user_item_matrix = pd.DataFrame(data=user_item_matrix)
     #user_item_matrix.to_csv(path_or_buf='/home/nick/Desktop/thesis/datasets/cosmetics-shop-data/user_item_matrix.csv')
+    #print(user_item_matrix)
+
+    user_item_matrix = pd.read_csv('/home/nick/Desktop/thesis/datasets/cosmetics-shop-data/user_item_matrix.csv')
     print(user_item_matrix)
+    user_item_matrix.rename(columns={"event_type.1": "rating"}, inplace=True)
+    user_item_matrix['rating'].astype(float)
+    ratings = build_dataset_with_ratings(user_item_matrix, 10000)
+    print(ratings)
 
-    # Get event types
-    event_types = data['event_type'].unique()
-    print(event_types)
-    # Get frequences of event types
-    event_types_freq = data['event_type'].value_counts()
-    print(event_types_freq)
-    event_types_freq = pd.DataFrame(event_types_freq)
-    event_types_freq.rename(columns={"event_type": "frequencies"}, inplace=True)
-    event_types_freq.index.name = 'event_type'
-    print(event_types_freq)
 
-    # visualize frequencies
-    plt.xlabel('Event Type')
-    plt.ylabel('Number of Events in Dataset')
-    plt.bar(range(len(event_types_freq)), event_types_freq['frequencies'])
-    plt.xticks(range(len(event_types_freq)), event_types, rotation='vertical')
-    plt.show()
+
+    x = ratings.values  # returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0,5))
+    x_scaled = min_max_scaler.fit_transform(x)
+    x_scaled = np.array(x_scaled)
+    ratings['rating'] = x_scaled.round(2)
+    print(ratings)
+    ratings.to_csv('/home/nick/Desktop/thesis/datasets/cosmetics-shop-data/ratings.csv')
+
+
+
+
 
 
 
