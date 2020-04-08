bf6b37951598326c721f5331c8bcb190986f58b2
nggianno <nick_gianno@hotmail.com>
2020-04-08 17:13:04 +0300

    update compute_dwelltime.py

--- preprocess-data/compute_dwelltime.py
+++ preprocess-data/compute_dwelltime.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import datetime
 
 
 userkey = 'user_id' # or 'user_session'
@@ -47,3 +48,80 @@ def get_session_duration_arr(data,userkey,timekey):
         lambda x: max(x) - min(x)).to_frame().rename(columns={timekey: 'Duration'})
     df = pd.DataFrame(df)
     return df
+
+if __name__ == "__main__":
+
+    #PATH = '/home/nick/Desktop/thesis/datasets/cosmetics-shop-data/brand_dataset.csv'
+    PATH = '/home/nick/Desktop/thesis/datasets/pharmacy-data/cleaned_users_products.csv'
+    userkey = 'user_id'
+    itemkey = 'product_id'
+    timekey = 'timestamp'
+
+    df = pd.read_csv(PATH)
+    df.drop(columns=['Unnamed: 0'],inplace=True)
+    print(df)
+    df.sort_values(by=[userkey,timekey],inplace=True)
+    df[timekey] = pd.to_datetime(df[timekey])
+    df[timekey]=df[timekey].dt.tz_localize(None)
+    print(df)
+    # df[timekey] = datetime.datetime.strptime()
+    # print(df)
+    """Compute dwelltime"""
+    dw_t = compute_dwell_time(df,userkey,itemkey,timekey)
+    dw_t = pd.DataFrame(dw_t)
+
+    final_df = df
+
+    """The last clickstream info of every session has invalid dwelltime due to the substraction of 2 different time periods
+    So we will clean these outliers and will take the average dwelltime of the session instead"""
+
+    """Calculate the average dwelltime of the records, ignoring outliers"""
+    cleaned_dwt = dw_t[(dw_t[timekey] != '0 days 00:00:00') & (dw_t[timekey] < '0 days 01:00:00')]
+    #cleaned_dwt = dw_t[dw_t[timekey] != '0 days 00:00:00']
+    cleaned_dwt.rename(columns = {"timestamp": "dwelltime"},inplace=True)
+    print(cleaned_dwt)
+
+    av_dwt = cleaned_dwt['dwelltime'].mean()
+    print(av_dwt)
+
+
+    """Replace last click duration of each session with the average dwelltime"""
+    dw_t.loc[(dw_t[timekey] == '0 days 00:00:00') ^ (dw_t[timekey] >= '0 days 01:00:00')] = av_dwt
+    #dw_t['timestamp'].apply(lambda x: av_dwt if x == '00:00:00' else x)
+    dw_t.rename(columns = {"timestamp": "dwelltime"},inplace=True)
+    # print(type(dw_t['dwelltime'][2]))
+    print(dw_t)
+    #print(final_df)
+    final_df['dwelltime'] = dw_t['dwelltime']
+    final_df = pd.DataFrame(final_df)
+    dwelltime_freqs = pd.DataFrame(final_df['dwelltime'].value_counts())
+
+    #Extract to csv
+    final_df.to_csv('/home/nick/Desktop/thesis/datasets/pharmacy-data/user_products_dwelltime.csv')
+
+    """Calculate mean dwelltime for each session"""
+
+    # mean_dwells = pd.DataFrame(merged_df1[['user_id','dwelltime']].groupby(userkey).mean(numeric_only=False))
+    # print(mean_dwells)
+    # # mean_dwells.reset_index(inplace=True)
+    # # print(mean_dwells)
+    # print(mean_dwells.loc[merged_df1[userkey][1],'dwelltime'])
+    #
+    #
+    # merged_df2 = pd.concat([df, cleaned_dwt], axis=1)
+    # #print(merged_df2)
+    # print(merged_df2['dwelltime'][2])
+
+
+    # for i in range(len(merged_df2)):
+    #     if pd.isna(merged_df2['dwelltime'][i]):
+    #         merged_df2['dwelltime'][i] = 1
+    #         #mean_dwells.loc[merged_df2[userkey][i], 'dwelltime']
+    # print(merged_df2)
+
+
+
+
+
+
+
