ec6574b04fbb9b6b3ee2deca667dc0a2ca66d175
nggianno <nick_gianno@hotmail.com>
2020-04-09 13:54:13 +0300

    add user_item_mapping.py

---
+++ preprocess-data/user_item_mapping.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+#Build mappings for indexed users, products, banners
+userkey = 'user_id'
+itemkey = 'product_id'
+timekey = 'timestamp'
+
+df = pd.read_csv('/home/nick/Desktop/thesis/datasets/pharmacy-data/rnn_data.csv')
+
+df.sort_values([userkey, timekey], inplace=True)
+
+userids = df[userkey].unique()
+useridmap = pd.Series(data=np.arange(len(userids)), index=userids)
+useridmap.index.names = ['user_id']
+print(useridmap)
+
+itemids = df[itemkey].unique()
+itemidmap = pd.Series(data=np.arange(len(itemids)), index=itemids)
+itemidmap.index.names = ['product_id']
+print(itemidmap)
+
